f396aa06357dcac453269639264224310777ab04
Chris Francis <chrisfrancis.dev@gmail.com>
2023-08-31 14:17:59 +0530

    making the code dry

--- image_search/image_search_app/views.py
+++ image_search/image_search_app/views.py
@@ -1,55 +1,65 @@
 from django.shortcuts import render
-from annoy import AnnoyIndex
-import tensorflow as tf
-import math
-from keras.applications.imagenet_utils import preprocess_input
+import sys
+from importlib import import_module
 from PIL import Image
 import os
 from django.conf import settings
 
-model = tf.keras.applications.ResNet50(weights='imagenet', include_top=False, pooling='avg')
+# Get the path to the directory containing views.py
+current_dir = os.path.dirname(__file__)
 
-batch_size = 64
-root_dir = os.path.join(settings.MEDIA_ROOT, 'My_DataSet')
+# Calculate the path to base.py using the parent directory
+base_path = os.path.join(current_dir, "..", "..", "base.py")
 
-# Create the data generator
-datagen = tf.keras.preprocessing.image.ImageDataGenerator(preprocessing_function=preprocess_input)
-generator = datagen.flow_from_directory(root_dir, target_size=(224, 224), batch_size=batch_size, class_mode=None, shuffle=False)
+# Normalize the path
+base_path = os.path.normpath(base_path)
 
-num_images = len(generator.filenames)
-num_epochs = int(math.ceil(num_images / batch_size))
+# Extract the directory and module name from the path
+base_dir, base_module = os.path.split(base_path)
+base_module = os.path.splitext(base_module)[0]
 
-# # Obtain the feature vectors using the pre-trained model
-# feature_list = model.predict(generator, num_epochs)
+# Add the base directory to the sys.path
+sys.path.append(base_dir)
 
-# # Flatten the spatial dimensions of the feature vectors
-# flatten_feature_list = [feature.flatten() for feature in feature_list]
+# Import base.py as a module
+base = import_module(base_module)
+######################################
+gen = base.base_code(1)
 
-# # Create and build the Annoy index
-# annoy_index = AnnoyIndex(2048, 'angular')
-# for i, feature in enumerate(flatten_feature_list):
-#     annoy_index.add_item(i, feature)
-# annoy_index.build(40)
+root_dir = next(gen)
+tf = next(gen)
+model = next(gen)
+generator = next(gen)
+num_epochs = next(gen)
+annoy_index = next(gen)
+
+# model = tf.keras.applications.ResNet50(weights='imagenet', include_top=False, pooling='avg')
+
+# batch_size = 64
+# root_dir = os.path.join(settings.MEDIA_ROOT, 'My_DataSet')
+
+# # Create the data generator
+# datagen = tf.keras.preprocessing.image.ImageDataGenerator(preprocessing_function=preprocess_input)
+# generator = datagen.flow_from_directory(root_dir, target_size=(224, 224), batch_size=batch_size, class_mode=None, shuffle=False)
+
+# num_images = len(generator.filenames)
+# num_epochs = int(math.ceil(num_images / batch_size))
 
 # Construct the full path to the .ann file
-annoy_index_path = os.path.join(os.path.dirname(__file__), '..', 'media', 'My_DataSet', 'annoy_caltech101index.ann')
+annoy_index_path = os.path.join(os.path.dirname(__file__), '..', 'media', 'My_DataSet', 'annoy_index.ann')
 
 # Load the Annoy index
-annoy_index = AnnoyIndex(2048, 'angular')
 annoy_index.load(annoy_index_path)
 
 def home(request):
     if request.method == 'POST' and request.FILES.get('image'):
-        image_file = request.FILES['image']
-        external_image = Image.open(image_file)
-        external_image = external_image.resize((224, 224))
-        external_image_array = preprocess_input(tf.keras.preprocessing.image.img_to_array(external_image))
-
-        # Obtain the feature vector for the external image
-        external_feature = model.predict(tf.expand_dims(external_image_array, axis=0)).flatten()
-
+        # image_file = request.FILES['image']
+        # external_image = Image.open(image_file)
+        # external_image = external_image.resize((224, 224))
+        # external_image_array = preprocess_input(tf.keras.preprocessing.image.img_to_array(external_image))
+        external_feature = base.process_single_image(image_file)
         # Retrieve the nearest neighbors for the specific external image
-        result = annoy_index.get_nns_by_vector(external_feature, 9, include_distances=True)
+        result = annoy_index.get_nns_by_vector(external_feature, 6, include_distances=True)
         print(result)
         # Get the class and image name for nearest neighbors
         nearest_neighbors_indices = result[0]
